e88b8000adc1ac26d99388e111f57043cadf53a5
kredo15 <kredo1511@yandex.ru>
2025-03-14 14:47:24 +0300

    view_get

--- backendbot/backendbot/urls.py
+++ backendbot/backendbot/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get_word/', views.WordForLearn.as_view)
 ]
--- backendbot/bot/views.py
+++ backendbot/bot/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from rest_fraemwork import APIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from . import models
-from django.db.models import Prefetch
+from django.db.models import OuterRef, Subquery
 
 
 class WordForLearn(APIView):
     def get(self, *args, **kwargs): 
-        """select * from dictionary A
-            LEFT JOIN Userdictionaries B ON A.id = B.word
-            WHERE B.user = user_id AND A.category = category"""
-        word = models.Dictionary.objects.prefetch_related(Prefetch)#(category=kwargs.get('name_category'))[:1]
-
+        """select * from dictionary
+            WHERE category = category AND id not in
+            (SELECT word FROM Userdictionaries WHERE user = user_id)"""
+        words_in_learn = models.Userdictionaries.objects.filter(user=kwargs.get('user_id'))
+        word = models.Dictionary.objects.annotate(word=Subquery(words_in_learn)).filter(category=kwargs.get('name_category')).values('en_word', 'ru_word')[:1]
+        return Response(word)
