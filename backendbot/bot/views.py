61d61f2f184e3a7150ba3ca8351aa01bbe353447
kredo15 <kredo1511@yandex.ru>
2025-03-14 10:19:00 +0300

    first_view

--- backendbot/bot/views.py
+++ backendbot/bot/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_fraemwork import APIView
+from . import models
+from django.db.models import Prefetch
 
-# Create your views here.
+
+class WordForLearn(APIView):
+    def get(self, *args, **kwargs): 
+        """select * from dictionary A
+            LEFT JOIN Userdictionaries B ON A.id = B.word
+            WHERE B.user = user_id AND A.category = category"""
+        word = models.Dictionary.objects.prefetch_related(Prefetch)#(category=kwargs.get('name_category'))[:1]
+
