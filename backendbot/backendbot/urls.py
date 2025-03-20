db77b9be7552b271253dff90e337bf6fc496f54b
Kredo15 <kredo1511@yandex.ru>
2025-03-20 22:52:59 +0300

    word_post

--- backendbot/backendbot/urls.py
+++ backendbot/backendbot/urls.py
@@ -20,5 +20,5 @@ from bot import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('get_new_word/', views.WordForLearn.as_view()) #, {'user_id': 1, 'name_category': 'популярные слова'}
+    path('get_new_word/', views.WordForLearn.as_view())#, {'user_id': 1, 'name_category': 'популярные слова'}
 ]
--- backendbot/bot/serializers.py
+++ backendbot/bot/serializers.py
@@ -30,9 +30,7 @@ class DictionaryModelSerializer(serializers.ModelSerializer):
         fields = ['enword', 'ruword', 'category']
 
     def create(self, validated_data):
-        category_create = validated_data.pop('category').pop('name')
-        enword_create = validated_data.pop('enword').pop('word')
-        ruword_create = validated_data.pop('ruword').pop('word')
-        dict_a_post = Dictionary.objects.create(category=category_create, enword=enword_create, ruword=ruword_create)
-        print(dict_a_post.query)
-        return super().create(validated_data)
+        category_create, _ = Categories.objects.get_or_create(name=validated_data.get('category').get('name'))
+        ruword_create, _ = Ruwords.objects.get_or_create(word=validated_data.get('enword').get('word'))
+        enword_create, _ = Enwords.objects.get_or_create(word=validated_data.get('ruword').get('word'))
+        return Dictionary.objects.create(category=category_create, enword=enword_create, ruword=ruword_create)
